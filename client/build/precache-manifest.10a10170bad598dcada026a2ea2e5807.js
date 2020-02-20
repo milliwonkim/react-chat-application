4c8d8eb113d36933fd09e90271129d46e90004fe
milliwonkim <rldnjs9347@gmail.com>
2020-02-20 11:38:14 +0900

    first commit

---
+++ client/build/precache-manifest.10a10170bad598dcada026a2ea2e5807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991d3f6533278e48d3863a99c9b8e8e2",
+    "url": "/react-chat-application/index.html"
+  },
+  {
+    "revision": "e37438cde8fbcd26cf58",
+    "url": "/react-chat-application/static/css/main.052acc05.chunk.css"
+  },
+  {
+    "revision": "114370130fbc44758da3",
+    "url": "/react-chat-application/static/js/2.68850bce.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/react-chat-application/static/js/2.68850bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37438cde8fbcd26cf58",
+    "url": "/react-chat-application/static/js/main.3d11f978.chunk.js"
+  },
+  {
+    "revision": "59cf913c1d28079d9b27",
+    "url": "/react-chat-application/static/js/runtime-main.9dab4f7d.js"
+  }
+]);
